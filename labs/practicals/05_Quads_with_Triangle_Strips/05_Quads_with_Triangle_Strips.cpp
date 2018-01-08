b80e00f81b5c91607dea6203bef439d3cf225992
Parsley <40206116@live.napier.ac.uk>
2018-01-08 15:52:33 +0000

    Week 1

--- labs/practicals/05_Quads_with_Triangle_Strips/05_Quads_with_Triangle_Strips.cpp
+++ labs/practicals/05_Quads_with_Triangle_Strips/05_Quads_with_Triangle_Strips.cpp
@@ -18,7 +18,7 @@ bool load_content() {
   vector<vec3> positions{
       // *********************************
       // Add the position data for two triangles here
-	  vec3(0.0f,0.5f,0.0f),vec3(1.0f,0.0f,)
+	  vec3(0.0f,0.5f,0.0f),vec3(1.0f,0.0f,0.0f),vec3(0.5f,1.0f,0.0f),vec3(2.0f,1.0f,0.0f),vec3(2.0f,0.0f,0.0f),vec3(2.5f,1.0f,0.0f),vec3(3.0f,0.5f,0.0f)
       // *********************************
   };
   // Colours
