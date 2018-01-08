d7c45f0988344f479294434d5d41efedb3b3d7df
Parsley <40206116@live.napier.ac.uk>
2018-01-08 15:37:50 +0000

    Week 1

--- labs/practicals/04_Quads_with_Quads/04_Quads_with_Quads.cpp
+++ labs/practicals/04_Quads_with_Quads/04_Quads_with_Quads.cpp
@@ -12,7 +12,7 @@ target_camera cam;
 bool load_content() {
   // *********************************
   // Set geometry type to quads
-
+	geom.set_type(GL_QUADS);
   // *********************************
   // Create quad data
   // Positions
