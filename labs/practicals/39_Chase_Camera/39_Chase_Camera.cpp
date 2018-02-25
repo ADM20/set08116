d31f46301e45073d2c29043f0b22a031b1cd7ac1
AP\Adam <40206116@student.napier.ac.uk>
2018-02-25 20:01:49 +0000

    49

--- labs/practicals/39_Chase_Camera/39_Chase_Camera.cpp
+++ labs/practicals/39_Chase_Camera/39_Chase_Camera.cpp
@@ -15,9 +15,9 @@ double cursor_y = 0.0;
 bool initialise() {
   // *********************************
   // Set input mode - hide the cursor
-
+	glfwSetInputMode(renderer::get_window(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
   // Capture initial mouse position
-
+	glfwGetCursorPos(renderer::get_window(), &cursor_x, &cursor_y);
   // *********************************
 
   return true;
@@ -88,47 +88,72 @@ bool update(float delta_time) {
   double current_y;
   // *********************************
   // Get the current cursor position
-
+  glfwGetCursorPos(renderer::get_window(), &current_x, &current_y);
   // Calculate delta of cursor positions from last frame
-
-
+  double delta_x = current_x - cursor_x;
+  double delta_y = current_y - cursor_y;
   // Multiply deltas by ratios and delta_time - gets actual change in orientation
-
-
+  delta_x *= ratio_width;
+  delta_y *= ratio_height;
   // Rotate cameras by delta
   // x - delta_y
   // y - delta_x
   // z - 0
-
+  cam.rotate(vec3(-delta_y, -delta_x, 0));
   // Use keyboard to rotate target_mesh - QE rotate on y-axis
-
-
-
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_Q))
+  {
+	  target_mesh.get_transform().rotate(vec3(0.0f, pi<float>() * delta_time, 0.0f));
+  }
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_E))
+  {
+	  target_mesh.get_transform().rotate(vec3(0.0f, -pi<float>() * delta_time, 0.0f));
+  }
 
   // Use keyboard to move the target_mesh - WSAD
 
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_W))
+  {
+	 
+	  vec3 direction(cam.get_target() - cam.get_position());
+	  vec3 directionN = normalize(vec3(direction.x, 0, direction.z));
 
+	  target_mesh.get_transform().translate(directionN * 0.1f);
 
+  }
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_S))
+  {
+	  vec3 direction(cam.get_target() - cam.get_position());
+	  vec3 directionN = normalize(vec3(direction.x, 0, direction.z));
 
+	  target_mesh.get_transform().translate(directionN * -0.1f);
+  }
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_A))
+  {
+	  vec3 direction(cam.get_target() - cam.get_position());
+	  vec3 directionN = normalize(vec3(direction.x, 0, direction.z));
+	  vec3 goRight = cross(directionN, cam.get_up());
 
+	  target_mesh.get_transform().translate(goRight * -0.1f);
 
+  }
+  if (glfwGetKey(renderer::get_window(), GLFW_KEY_D))
+  {
+	  vec3 direction(cam.get_target() - cam.get_position());
+	  vec3 directionN = normalize(vec3(direction.x, 0, direction.z));
+	  vec3 goRight = cross(directionN, cam.get_up());
 
+	  target_mesh.get_transform().translate(goRight * 0.1f);
 
-
-
-
-
-
-
+  }
 
 
   // Move camera - update target position and rotation
-
+  cam.move(meshes["chaser"].get_transform().position, eulerAngles(meshes["chaser"].get_transform().orientation));
   // Update the camera
-
+  cam.update(delta_time);
   // Update cursor pos
-
-
+  glfwGetCursorPos(renderer::get_window(), &cursor_x, &cursor_y);
   // *********************************
   return true;
 }
